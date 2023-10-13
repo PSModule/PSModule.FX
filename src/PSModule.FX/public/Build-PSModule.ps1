04ec28053e333e0c064f0a92f0c8c3b4b170a967
Marius Storhaug <marstor@hotmail.com>
2023-10-13 22:29:21 +0200

    Add requirements

--- src/PSModule.FX/public/Build-PSModule.ps1
+++ src/PSModule.FX/public/Build-PSModule.ps1
@@ -1,4 +1,6 @@
-﻿function Build-PSModule {
+﻿#Requires -Modules platyPS, PowerShellGet, PackageManagement
+
+function Build-PSModule {
 [CmdletBinding(SupportsShouldProcess)]
 param(
     $Path = 'src'
@@ -7,62 +9,6 @@ param(
 $task = New-Object System.Collections.Generic.List[string]
 $task.Add('Build-Module')
 Write-Output "::group::[$($task -join '] - [')] - Starting..."
-
-#region Install-Prerequisites
-$task.Add('Install-Prerequisites')
-Write-Output "::group::[$($task -join '] - [')]"
-
-$prereqModuleNames = 'platyPS', 'PowerShellGet', 'PackageManagement'
-Write-Verbose "[$($task -join '] - [')] - Found $($prereqModuleNames.Count) modules"
-$prereqModuleNames | ForEach-Object { Write-Verbose "[$($task -join '] - [')] - [$_]" }
-
-foreach ($prereqModuleName in $prereqModuleNames) {
-    $task.Add($prereqModuleName)
-    Write-Output "::group::[$($task -join '] - [')]"
-
-    $availableModule = Find-Module -Name $prereqModuleName | Sort-Object -Property Version -Descending | Select-Object -First 1
-    $isAvailable = $availableModule.count -gt 0
-    Write-Output "::group::[$($task -join '] - [')] - Available - [$isAvailable]"
-    $availableModuleVersion = $availableModule.Version
-    Write-Output "::group::[$($task -join '] - [')] - Available - Version - [$availableModuleVersion]"
-
-    $installedPrereqModule = Get-Module -ListAvailable -Name $prereqModuleName | Sort-Object -Property Version -Descending | Select-Object -First 1
-    $isInstalled = $installedPrereqModule.count -gt 0
-    Write-Output "::group::[$($task -join '] - [')] - Installed - [$isInstalled]"
-    $installedPrereqModuleVersion = $installedPrereqModule.Version
-    Write-Output "::group::[$($task -join '] - [')] - Installed - Version - [$installedPrereqModuleVersion]"
-
-    if ($isInstalled) {
-        if ($installedPrereqModuleVersion -lt $availableModuleVersion) {
-            Write-Output "::group::[$($task -join '] - [')] - Updating - Version - [$installedPrereqModuleVersion] -> [$availableModuleVersion]"
-            Install-Module -Name $prereqModuleName -Scope CurrentUser -Force
-        }
-    } else {
-        Write-Output "::group::[$($task -join '] - [')] - Installing - Version - [$availableModuleVersion]"
-        $availableModule | Install-Module -Scope CurrentUser -Force
-    }
-
-    $isLoaded = (Get-Module | Where-Object -Property Name -EQ $prereqModuleName).count -gt 0
-    if ($isLoaded) {
-        Write-Output "::group::[$($task -join '] - [')] - Imported"
-    } else {
-        Write-Output "::group::[$($task -join '] - [')] - Importing to session"
-
-        try {
-            Import-Module -Name $prereqModuleName -Force -ErrorAction SilentlyContinue
-        } catch {}
-    }
-    Write-Output "::group::[$($task -join '] - [')] - Done"
-    $task.RemoveAt($task.Count - 1)
-    Write-Output '::endgroup::'
-}
-
-Write-Output "::group::[$($task -join '] - [')] - Done"
-Get-InstalledModule | Select-Object Name, Version, Author | Sort-Object -Property Name | Format-Table -AutoSize
-
-$task.RemoveAt($task.Count - 1)
-Write-Output '::endgroup::'
-#endregion Install-Prerequisites
 
 #region Process-Module
 $task.Add('Process-Module')
