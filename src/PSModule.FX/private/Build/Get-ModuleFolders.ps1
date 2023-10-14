12f82f8fb47c62ddf8775579633cb0779b94bb2a
Marius Storhaug <marstor@hotmail.com>
2023-10-14 15:05:15 +0200

    Refactor Build-PSModule

---
+++ src/PSModule.FX/private/Build/Get-ModuleFolders.ps1
@@ -0,0 +1,29 @@
+﻿function Get-ModuleFolders {
+    <#
+        .SYNOPSIS
+        Get all folders where the content of the folder is a module file or manifest file.
+
+        .DESCRIPTION
+        Get all folders where the content of the folder is a module file or manifest file.
+        Search is recursive.
+
+        .EXAMPLE
+        Get-ModuleFolders -Path 'src'
+
+        Get all folders where the content of the folder is a module file or manifest file.
+    #>
+    [CmdletBinding()]
+    param(
+        # Path to the folder where the modules are located.
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string] $Path = 'src'
+    )
+
+    $moduleFolders = Get-ChildItem -Path $Path -Directory -Recurse -ErrorAction SilentlyContinue | Where-Object {
+        Get-ChildItem -Path $_.FullName -File -ErrorAction SilentlyContinue | Where-Object {
+            $_.Name -match '.*\.psm1|.*\.psd1'
+        }
+    }
+    return $moduleFolders
+}
