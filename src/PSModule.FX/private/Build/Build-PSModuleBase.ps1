23b3ec4cacb299c2354606e96fe38b10f6be9fa8
Marius Storhaug <marstor@hotmail.com>
2023-10-14 22:03:27 +0200

    Copy only needed files to moduleoutputfolder

--- src/PSModule.FX/private/Build/Build-PSModuleBase.ps1
+++ src/PSModule.FX/private/Build/Build-PSModuleBase.ps1
@@ -10,6 +10,9 @@
         [string] $OutputFolderPath
     )
 
+    $sourceFolder = Get-Item -Path $SourceFolderPath
+    $outputFolder = Get-Item -Path $OutputFolderPath
+
     $moduleName = Split-Path -Path $SourceFolderPath -Leaf
     Write-Output "::group::[$moduleName] - Build base"
 
@@ -17,17 +20,26 @@
         'init',
         'private',
         'public',
-        "$moduleName.psd1"
+        "$moduleName.psd1",
+        "$moduleName.psm1"
     )
     Write-Verbose "Copying files from [$SourceFolderPath] to [$OutputFolderPath]"
     $ignorePaths | ForEach-Object {
-        Write-Verbose "Ignoring path [$_]"
+        Write-Verbose "Ignoring path [$_]" -Verbose
     }
 
-    Copy-Item -Path "$moduleFolder/*" -Destination $OutputFolderPath -Recurse -Verbose
+    $filePaths = New-Object System.Collections.Generic.List[string]
+    $filePaths += Get-ChildItem -Path $sourceFolder.FullName -Directory -Exclude $ignorePaths | Get-ChildItem -Recurse -File | Select-Object -ExpandProperty FullName
+    $filePaths += Get-ChildItem -Path $sourceFolder.FullName -File -Exclude $ignorePaths | Select-Object -ExpandProperty FullName
+    $filePaths = $filePaths | Sort-Object
+    $filePaths | ForEach-Object {
+        $sourceFilePath = $_
+        $destinationFilePath = $_ -replace ($sourceFolder.FullName), ($outputFolder.FullName)
+        Copy-Item -Path $sourceFilePath -Destination $destinationFilePath -Force -Verbose
+    }
     Write-Output '::endgroup::'
 
-    "::group::[$moduleName] - Build base - Result"
+    Write-Output "::group::[$moduleName] - Build base - Result"
     (Get-ChildItem -Path $OutputFolderPath -Recurse -Force).FullName | Sort-Object
     Write-Output '::endgroup::'
 }
