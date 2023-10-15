fb8ef6699e45fafb259899d8d29e5988f907026d
Marius Storhaug <marstor@hotmail.com>
2023-10-15 09:47:10 +0200

    Added function to get functions and filters to export

---
+++ src/PSModule.FX/private/Build/Get-PSModuleFunctionsToExport.ps1
@@ -0,0 +1,26 @@
+﻿function Get-PSModuleFunctionsToExport {
+    [CmdletBinding()]
+    param(
+        # Path to the folder where the module source code is located.
+        [Parameter(Mandatory)]
+        [string] $SourceFolderPath
+    )
+
+    $moduleName = Split-Path -Path $SourceFolderPath -Leaf
+    $manifestPropertyName = 'FunctionsToExport'
+
+    Write-Verbose "[$moduleName] - [$manifestPropertyName]"
+    Write-Verbose "[$moduleName] - [$manifestPropertyName] - Checking path for functions and filters"
+
+    $publicFolderPath = Join-Path $SourceFolderPath 'public'
+    Write-Verbose "[$moduleName] - [$manifestPropertyName] - [$publicFolderPath]"
+    $functionsToExport = Get-ChildItem -Path $publicFolderPath -Recurse -File -ErrorAction SilentlyContinue -Include '*.ps1' | ForEach-Object {
+        $fileContent = Get-Content -Path $_.FullName -Raw
+        $containsFunction = ($fileContent -match 'function ') -or ($fileContent -match 'filter ')
+        Write-Verbose "[$moduleName] - [$manifestPropertyName] - [$($_.BaseName)] - [$containsFunction]"
+        $containsFunction ? $_.BaseName : $null
+    }
+    $functionsToExport = $functionsToExport.count -eq 0 ? @() : @($functionsToExport)
+
+    $functionsToExport
+}
