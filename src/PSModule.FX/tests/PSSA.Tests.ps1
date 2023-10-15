56284a80eb79952b7423e7b2085f51028a44dedb
Marius Storhaug <marstor@hotmail.com>
2023-10-15 21:59:40 +0200

    test

--- src/PSModule.FX/tests/PSSA.Tests.ps1
+++ src/PSModule.FX/tests/PSSA.Tests.ps1
@@ -15,13 +15,9 @@ $scriptAnalyzerRules | ForEach-Object {
 $Severities = @('Information', 'Warning', 'Error')
 
 foreach ($Severity in $Severities) {
-
     Describe "Testing PSSA $Severity Rules" -Tag $Severity {
-
         It '<CommonName> (<RuleName>)' -ForEach ($Rules | Where-Object Severity -EQ $Severity) {
-
             param ($RuleName)
-
             #Test all scripts for the given rule and if there is a problem display this problem in a nice an reabable format in the debug message and let the test fail
             Invoke-ScriptAnalyzer -Path $Path -IncludeRule $RuleName -Recurse |
                 ForEach-Object {
