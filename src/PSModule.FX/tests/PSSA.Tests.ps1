8cfc51bd681e794d19824c221589e8f167e8ff2b
Marius Storhaug <marstor@hotmail.com>
2023-10-15 22:01:36 +0200

    test

--- src/PSModule.FX/tests/PSSA.Tests.ps1
+++ src/PSModule.FX/tests/PSSA.Tests.ps1
@@ -5,18 +5,14 @@ Param(
 )
 
 # Get all PSScript Analyzer Rules and save them in an array
-$scriptAnalyzerRules = Get-ScriptAnalyzerRule
-$Rules = @()
-$scriptAnalyzerRules | ForEach-Object {
-    $Rules += @{'RuleName' = $_.RuleName; 'Severity' = $_.Severity }
-}
+$rules = Get-ScriptAnalyzerRule
 
 # Create an array of the types of rules
 $Severities = @('Information', 'Warning', 'Error')
 
 foreach ($Severity in $Severities) {
     Describe "Testing PSSA $Severity Rules" -Tag $Severity {
-        It '<CommonName> (<RuleName>)' -ForEach ($Rules | Where-Object Severity -EQ $Severity) {
+        It '<CommonName> (<RuleName>)' -ForEach ($rules | Where-Object Severity -EQ $Severity) {
             param ($RuleName)
             #Test all scripts for the given rule and if there is a problem display this problem in a nice an reabable format in the debug message and let the test fail
             Invoke-ScriptAnalyzer -Path $Path -IncludeRule $RuleName -Recurse |
