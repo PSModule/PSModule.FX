fa9d1ff4943592ab138daca814714bb54a4a7003
Marius Storhaug <marstor@hotmail.com>
2023-10-15 09:44:31 +0200

    Added get the module manifest

---
+++ src/PSModule.FX/private/Build/Get-PSModuleManifest.ps1
@@ -0,0 +1,46 @@
+﻿function Get-PSModuleManifest {
+    [CmdletBinding()]
+    param(
+        # Path to the folder where the module source code is located.
+        [Parameter(Mandatory)]
+        [string] $SourceFolderPath,
+
+        # The format of the output
+        [Parameter()]
+        [ValidateSet('FileName', 'FilePath', 'FileInfo', 'Content', 'Hashtable')]
+        [string] $As = 'Hashtable'
+    )
+
+    $moduleName = Split-Path -Path $SourceFolderPath -Leaf
+    $manifestPropertyName = 'ManifestFile'
+
+    Write-Verbose "[$moduleName] - [$manifestPropertyName]"
+    $manifestFileName = "$moduleName.psd1"
+    Write-Verbose "[$moduleName] - [$manifestPropertyName] - [$manifestFileName]"
+    Write-Verbose "[$moduleName] - [$manifestPropertyName] - Checking path for manifest file"
+
+    $manifestFilePath = Join-Path -Path $SourceFolderPath $manifestFileName
+    if (-not (Test-Path -Path $manifestFilePath)) {
+        Write-Warning "[$manifestFileName] - 🟥 No manifest file found"
+        return $null
+    }
+    Write-Verbose "[$manifestFileName] - 🟩 Found manifest file"
+
+    switch ($As) {
+        'FileName' {
+            return $manifestFileName
+        }
+        'FilePath' {
+            return $manifestFilePath
+        }
+        'FileInfo' {
+            return Get-Item -Path $manifestFilePath
+        }
+        'Content' {
+            return Get-Content -Path $manifestFilePath
+        }
+        'Hashtable' {
+            return Import-PowerShellDataFile -Path $manifestFilePath
+        }
+    }
+}
