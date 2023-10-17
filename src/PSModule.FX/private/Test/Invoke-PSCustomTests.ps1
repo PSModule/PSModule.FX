f4b1745928c6293e4816b9afea0a9ceb0c96572f
Marius Storhaug <marstor@hotmail.com>
2023-10-17 21:13:21 +0200

    Add function for custom tests

---
+++ src/PSModule.FX/private/Test/Invoke-PSCustomTests.ps1
@@ -0,0 +1,48 @@
+﻿function Invoke-PSCustomTests {
+    [CmdletBinding()]
+    param(
+        # Path to the folder where the built modules are outputted.
+        [Parameter(Mandatory)]
+        [string] $ModuleFolder,
+
+        # Path to the folder where the built modules are outputted.
+        [Parameter(Mandatory)]
+        [string] $TestFolderPath
+    )
+
+    $containerParams = @{
+        Path = $TestFolderPath
+        Data = @{
+            Path = $ModuleFolder
+        }
+    }
+    Write-Verbose 'ContainerParams:'
+    Write-Verbose "$($containerParams | ConvertTo-Json -Depth 5)"
+
+
+    $pesterParams = @{
+        Configuration = @{
+            Run        = @{
+                Container = New-PesterContainer @containerParams
+                PassThru  = $false
+            }
+            TestResult = @{
+                TestSuiteName = 'PSSA'
+                OutputPath    = '.\outputs\CustomTest.Results.xml'
+                OutputFormat  = 'NUnitXml'
+                Enabled       = $true
+            }
+            Output     = @{
+                Verbosity           = 'Detailed'
+                StackTraceVerbosity = 'None'
+            }
+        }
+        ErrorAction   = 'Stop'
+    }
+
+    Write-Verbose 'PesterParams:'
+    Write-Verbose "$($pesterParams | ConvertTo-Json -Depth 5)"
+
+    Invoke-Pester @pesterParams
+
+}
