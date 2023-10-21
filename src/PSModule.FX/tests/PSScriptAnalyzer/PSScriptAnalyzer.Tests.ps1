651083a1d13b1540c639f70da18ed63f73f8a6f8
Marius Storhaug <marstor@hotmail.com>
2023-10-21 15:35:57 +0200

    Fixing tests

--- src/PSModule.FX/tests/PSScriptAnalyzer/PSScriptAnalyzer.Tests.ps1
+++ src/PSModule.FX/tests/PSScriptAnalyzer/PSScriptAnalyzer.Tests.ps1
@@ -7,33 +7,26 @@ Param(
     [string] $SettingsFilePath
 )
 
-BeforeAll {
-    $rules = Get-ScriptAnalyzerRule | Sort-Object -Property Severity
+BeforeDiscovery {
+    $rules = Get-ScriptAnalyzerRule | Sort-Object -Property Severity -Verbose:$false | ConvertTo-Json | ConvertFrom-Json -AsHashtable
     Write-Warning "Discovered [$($rules.Count)] rules"
-    $testResults = Invoke-ScriptAnalyzer -Path $Path -Settings $SettingsFilePath -Recurse
-    Write-Warning "Found [$($testResults.Count)] issues"
+    $relativeSettingsFilePath = $SettingsFilePath.Replace($PSScriptRoot, '').Trim('\').Trim('/')
 }
-# Line                 : 20
-# Column               : 21
-# Message              : Use space before and after binary and assignment operators.
-# Extent               : =
-# RuleName             : PSUseConsistentWhitespace
-# Severity             : Warning
-# ScriptName           : PSScriptAnalyzer.Tests.ps1
-# ScriptPath           : C:\Repos\GitHub\PSModule\Framework\PSModule.FX\src\PSModule.FX\tests\PSScriptAnalyzer\PSScriptAnalyzer.Tests.ps1
-# RuleSuppressionID    :
-# SuggestedCorrections : {Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent}
-# IsSuppressed         : False
 
-Describe "PSScriptAnalyzer tests using settings file [$SettingsFilePath]" {
+Describe "PSScriptAnalyzer tests using settings file [$relativeSettingsFilePath]" {
+    BeforeAll {
+        $testResults = Invoke-ScriptAnalyzer -Path $Path -Settings $SettingsFilePath -Recurse -Verbose:$false
+        Write-Warning "Found [$($testResults.Count)] issues"
+    }
+
     It '<CommonName> (<RuleName>)' -ForEach $rules {
         param ($RuleName)
 
-        $issues = $testResults | Where-Object -Property RuleName -eq $RuleName | ForEach-Object {
-            $relativePath = $_.ScriptPath.Replace($Path, '')
-            $suggestion = $_.SuggestedCorrections
-            "$([Environment]::NewLine)$relativePath`:L$($_.Line):C$($_.Column): $($_.Message)"
+        $issues = $testResults | Where-Object -Property RuleName -EQ $RuleName | ForEach-Object {
+            $relativePath = $_.ScriptPath.Replace($Path, '').Trim('\').Trim('/')
+            $message = $_.Message
+            "$([Environment]::NewLine)$relativePath`:L$($_.Line):C$($_.Column)"
         }
-        $issues | Should -BeNullOrEmpty -Because $suggestion
+        $issues | Should -BeNullOrEmpty -Because $message
     }
 }
---
+++ tools/Test-Locally.ps1
@@ -0,0 +1,39 @@
+﻿$root = $PSScriptRoot ? $PSScriptRoot : 'C:\Repos\GitHub\PSModule\Framework\PSModule.FX'
+
+
+$modulePath = (Join-Path -Path (Split-Path $root -Parent) -ChildPath 'src' 'PSModule.FX')
+$containerParams = @{
+    Path = (Join-Path -Path $modulePath -ChildPath 'tests' 'PSScriptAnalyzer' 'PSScriptAnalyzer.Tests.ps1')
+    Data = @{
+        Path             = $modulePath
+        SettingsFilePath = (Join-Path -Path $modulePath -ChildPath 'tests' 'PSScriptAnalyzer' 'PSScriptAnalyzer.Settings.psd1')
+    }
+}
+
+Write-Verbose 'ContainerParams:'
+Write-Verbose "$($containerParams | ConvertTo-Json -Depth 5)"
+
+$pesterParams = @{
+    Configuration = @{
+        Run        = @{
+            Container = New-PesterContainer @containerParams
+            PassThru  = $false
+        }
+        TestResult = @{
+            Enabled       = $true
+            TestSuiteName = 'PSScriptAnalyzer'
+            OutputPath    = (Join-Path -Path $root -ChildPath 'outputs' 'PSScriptAnalyzer.Results.xml')
+            OutputFormat  = 'NUnitXml'
+        }
+        Output     = @{
+            CIFormat            = 'Auto'
+            Verbosity           = 'Detailed'
+            StackTraceVerbosity = 'None'
+        }
+    }
+    ErrorAction   = 'Stop'
+}
+Write-Verbose 'PesterParams:'
+Write-Verbose "$($pesterParams | ConvertTo-Json -Depth 5)"
+
+Invoke-Pester @pesterParams
