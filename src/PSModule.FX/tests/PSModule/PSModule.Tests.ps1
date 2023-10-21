7172d05064b25f89d8a02ce2daff29e643e3a0ce
Marius Storhaug <marstor@hotmail.com>
2023-10-21 16:02:36 +0200

    tests

--- src/PSModule.FX/tests/PSModule/PSModule.Tests.ps1
+++ src/PSModule.FX/tests/PSModule/PSModule.Tests.ps1
@@ -1,5 +1,28 @@
-﻿Describe 'PSModule.FX' {
-    It 'Is a module' {
-        $true | Should -Be $true
+﻿[CmdLetBinding()]
+Param(
+    [Parameter(Mandatory)]
+    [string] $Path
+)
+BeforeDiscovery {
+    $scriptFiles = Get-ChildItem -Path $Path -Filter '*.ps1' -Recurse -File | ForEach-Object {
+        $fileContent = Get-Content -Path $_.FullName -Raw
+        if ($fileContent -match '(?:function|filter)\s+([a-zA-Z][a-zA-Z0-9-]*)') {
+            $functionName = $matches[1]
+            Write-Host "Function/filter name is: $functionName"
+            $item = @{
+                fileName     = $_.Name
+                filePath     = $_.FullName.Replace($Path, '').Trim('\').Trim('/')
+                functionName = $functionName
+            }
+            $item
+        }
     }
+}
+
+Describe 'Module design tests' {
+    It 'File name and function name should match' -ForEach $scriptFiles {
+        $fileName | Should -BeExactly $functionName -Because "Script files should be called the same as the function they contain"
+    }
+
+    # It 'Script file should only contain max one function or filter' {}
 }
