aa8dff830980d45990898a22dfc1a4e7a953dcbb
Marius Storhaug <marstor@hotmail.com>
2023-10-22 22:19:12 +0200

    test combinding all tests in one pester exec

--- src/PSModule.FX/private/Invoke-PSModuleTest.ps1
+++ src/PSModule.FX/private/Invoke-PSModuleTest.ps1
@@ -18,26 +18,83 @@
         [Parameter(Mandatory)]
         [string] $ModuleFolderPath
     )
-    $failedTests = 0
+    $containers = @()
     Write-Verbose "ModuleFolderPath - [$ModuleFolderPath]"
 
     $moduleName = Split-Path -Path $ModuleFolderPath -Leaf
     Write-Verbose "[$moduleName] - Invoke-ScriptAnalyzer"
-    $moduleFolder = Get-Item -Path $ModuleFolderPath
-    $failedTests += Invoke-PSScriptAnalyzerTest -ModuleFolder $moduleFolder -Verbose:$false
+    $containerParams = @{
+        Path = (Join-Path -Path $PSScriptRoot -ChildPath 'tests' 'PSScriptAnalyzer' 'PSScriptAnalyzer.Tests.ps1')
+        Data = @{
+            Path             = $ModuleFolderPath
+            SettingsFilePath = (Join-Path -Path $PSScriptRoot -ChildPath 'tests' 'PSScriptAnalyzer' 'PSScriptAnalyzer.Tests.psd1')
+        }
+    }
+    Write-Verbose 'ContainerParams:'
+    Write-Verbose "$($containerParams | ConvertTo-Json)"
+    $containers += New-PesterContainer @containerParams
 
     Write-Verbose "[$moduleName] - Invoke-PSCustomTests - PSModule defaults"
     $testFolderPath = Join-Path -Path $PSScriptRoot -ChildPath 'tests' 'PSModule'
-    $failedTests += Invoke-PSCustomTests -ModuleFolder $moduleFolder -TestFolderPath $TestFolderPath -Verbose:$false
+    $containerParams = @{
+        Path = $testFolderPath
+        Data = @{
+            Path = $ModuleFolderPath
+        }
+    }
+    Write-Verbose 'ContainerParams:'
+    Write-Verbose "$($containerParams | ConvertTo-Json -Depth 5)"
+    $containers += New-PesterContainer @containerParams
 
     Write-Verbose "[$moduleName] - Invoke-PSCustomTests - Specific tests"
     $testFolderPath = Join-Path -Path (Split-Path -Path (Split-Path -Path $ModuleFolderPath -Parent) -Parent) -ChildPath 'tests' $moduleName
     Write-Verbose "[$moduleName] - [$testFolderPath] - Checking for tests"
     if (Test-Path -Path $testFolderPath) {
-        $failedTests += Invoke-PSCustomTests -ModuleFolder $moduleFolder -TestFolderPath $testFolderPath -Verbose:$false
+        $containerParams = @{
+            Path = $testFolderPath
+            Data = @{
+                Path = $ModuleFolderPath
+            }
+        }
+        Write-Verbose 'ContainerParams:'
+        Write-Verbose "$($containerParams | ConvertTo-Json -Depth 5)"
+        $containers += New-PesterContainer @containerParams
     } else {
         Write-Warning "[$moduleName] - [$testFolderPath] - No tests found"
     }
+
+    $pesterParams = @{
+        Configuration = @{
+            Run          = @{
+                Container = $containers
+                PassThru  = $false
+            }
+            TestResult   = @{
+                Enabled       = $true
+                OutputFormat  = 'NUnitXml'
+                OutputPath    = '.\outputs\PSModuleTest.Results.xml'
+                TestSuiteName = 'PSModule Test'
+            }
+            CodeCoverage = @{
+                Enabled               = $true
+                OutputPath            = '.\outputs\CodeCoverage.xml'
+                OutputFormat          = 'JaCoCo'
+                OutputEncoding        = 'UTF8'
+                CoveragePercentTarget = 75
+            }
+            Output       = @{
+                CIFormat            = 'Auto'
+                StackTraceVerbosity = 'None'
+                Verbosity           = 'Detailed'
+            }
+        }
+    }
+
+    Write-Verbose 'PesterParams:'
+    Write-Verbose "$($pesterParams | ConvertTo-Json -Depth 5)"
+
+    Invoke-Pester @pesterParams
+    $failedTests = $LASTEXITCODE
 
     if ($failedTests -gt 0) {
         Write-Error "[$moduleName] - [$failedTests] tests failed"
